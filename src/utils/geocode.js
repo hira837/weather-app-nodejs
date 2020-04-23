0262eebfe5bbd12ae4ad78dafa8883016f545c01
Yuki Hirayama <gfyxm868@gmail.com>
2020-04-23 13:28:17 +0900

    Set up for heroku

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ const geocode = (address, callback) => {
   const url =
     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
     encodeURIComponent(address) +
-    ".json?access_token=pk.eyJ1IjoiaGlyYXlhbWEiLCJhIjoiY2s5ODZhb2gyMHhkYTNsb2NpdHVqYnBpMSJ9.nu8hG-p4HlaQOIVdUXxRFw&limit=1";
+    ".json?access_token=pk.eyJ1IjoiaGlyYXlhbWEiLCJhIjoiY2s5ODZhb2gyMHhkYTNsb2NpdHVqYnBpMSJ9.nu8hG-p4HlaQOIVdUXxRFw&limit=3";
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
